5a689b88cb3133dedbddd151719dc9f0068b202a
Tanner Eschmann <107972255+TannerEsch@users.noreply.github.com>
2023-04-05 04:38:41 -0400

    level and daily streak redone to be dynamic for incoming server data

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -6,6 +6,8 @@ import { Profile } from "../../types/models";
 import mainpic from './mainpic.png'
 
 import bg from './bg.png'
+import moon from './moon.png'
+import star from './star.png'
 
 // services
 import * as profileService from '../../Services/profileService'
@@ -38,13 +40,33 @@ const ProfilePage = (props: any) => {
 
       <div className="font-bold font-nunito text-2xl">
         <p>Level</p>
-        <img src="/slider-demo.png" alt="" />
+        <div className="bg-valvetNight h-16 w-[90vw] rounded-3xl">
+        <div className="flex justify-between px-4 items-center h-[100%]">
+          <img src={star}/>
+          <div className="bg-white w-[100%] mt-1 h-2">
+          <div className="bg-yellow-500 w-[15%] h-2"></div>
+          </div>
+          <img src={star} />
+        </div>
+        </div>
       </div>
 
-      <div className="font-bold font-nunito text-2xl">
-        <p>Daily Streak</p>
-        <img src="/week-challenge.png" alt="" />
-      </div>
+        <div>
+        <p className="foint-nunito text-2xl font-bold mr-auto mb-1">Daily Streak</p>
+        <div className="flex flex-col font-nunito items-center gap-1 text-white font-bold h-20 w-[90vw] rounded-3xl bg-valvetNight">
+        <div className="flex w-[100%] justify-evenly mt-4">
+        <p className="border-yellow-500 border-2 rounded-[50%] w-8 h-8 flex items-center justify-center">S</p>
+        <p className="border-yellow-500 border-2 rounded-[50%] w-8 h-8 flex items-center justify-center">M</p>
+        <p className="border-yellow-500 border-2 rounded-[50%] w-8 h-8 flex items-center justify-center">T</p>
+        <p className="border-yellow-500 border-2 rounded-[50%] w-8 h-8 flex items-center justify-center">W</p>
+        <p className="border-yellow-500 border-2 rounded-[50%] w-8 h-8 flex items-center justify-center">T</p>
+        <p className="border-yellow-500 border-2 rounded-[50%] w-8 h-8 flex items-center justify-center">F</p>
+        <p className="border-yellow-500 border-2 rounded-[50%] w-8 h-8 flex items-center justify-center">S</p>
+        </div>
+        <p className="text-lg">Keep up your daily progress!</p>
+        </div>
+        </div>
+
       <div className="font-bold font-nunito text-2xl">
         <h1>Achievements</h1>
         <img src="/achievements.png" alt="" />
